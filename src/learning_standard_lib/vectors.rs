c1a1ef655ee7a69d86abb463c58c065e530c5351
Blankfrack <bryan.benoit@gmail.com>
2019-11-19 19:15:36 -0500

    Put things in modules

---
+++ src/learning_standard_lib/vectors.rs
@@ -0,0 +1,9 @@
+pub fn test_vector() {
+    let mut vec = Vec::new();
+    vec.push("Hello");
+    vec.push("World");
+
+    for s in vec {
+        println!("{}", s);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// On défini les modules
+mod learning_ownership;
+mod learning_standard_lib;
+
+// On défini ce que nous allons utiliser dans les modules/librairies standard/librairie importées
+use learning_ownership::*;
+use learning_standard_lib::vectors::*;
+
+/// Main
 fn main() {
     test_move();
 
@@ -6,82 +15,6 @@ fn main() {
     test_pass_by_reference();
 
     test_shared_borrow();
+
+    test_vector();
 }
-
-fn test_move() {
-    // Déclaration d'une variable
-    let foo: i32 = 42;
-    println!("foo : {}", foo);
-
-    let my_string: String = String::from("Hello World");
-    println!("my_string : {}", my_string);
-
-    {
-        // Après le block, foo sera toujours accessible car type de base (implémente le trait Copy).  STACK
-        let foo2 = foo;
-        println!("foo dans le block : {}", foo);
-        println!("foo2 dans le bloc : {}", foo2);
-
-        // Move de variable. my_string ne sera plus accessible après le bloc car MOVED.  HEAP
-        let my_string2 = my_string;
-
-        // my_string a MOVED donc elle n'est plus accessible
-        //print!("my_string : {}", my_string);
-        println!("my_string2 : {}", my_string2);
-    }
-
-    // Impossible car on est sorti du bloc dans lequel foo2 a été déclarée
-    //println!("foo2 après block : {}", foo2);
-
-    // Toujours possible car foo est un type de base. Sa valeur est copié dans foo2 dans le bloc précédent
-    //  foo conserve sa valeur
-    println!("foo après le bloc : {}", foo);
-
-    // Impossible.  La variable my_string n'est plus accessible car on la moved dans le bloc en la passant à my_string2
-    //print!("my_string : {}", my_string);
-}
-
-fn test_pass_by_reference() {
-    let my_string3: String = String::from("my_string3");
-
-    // pass by reference. Keeps ownership
-    print_string_reference(&my_string3);
-
-    println!(
-        "my_string3 après call à print_string_reference : {}",
-        my_string3
-    );
-}
-
-fn test_pass_by_value() {
-    let my_string4: String = String::from("my_string4");
-
-    // pass by value and hence transfer ownership
-    print_string_value(my_string4);
-
-    // Impossible car moved
-    //println!("my_string4 après call à print_string_value : {}", my_string4);
-}
-
-fn print_string_value(p_string: String) {
-    println!("p_string : {}", p_string);
-}
-
-fn print_string_reference(p_string: &String) {
-    println!("p_string : {}", p_string);
-}
-
-fn test_shared_borrow() {
-    let mut name : String = String::from("Hello");
-    name.push_str(" World!");
-    
-    print_name(&name);
-    print_name(&name);
-}
-
-fn print_name(name : &String) {
-    //name.push_str("string: &str");
-    println!("{}", name);
-}
-
-
