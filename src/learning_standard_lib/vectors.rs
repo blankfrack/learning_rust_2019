25f0c9a4120d0a391ebf2d71e952aad00d136e7e
Blankfrack <bryan.benoit@gmail.com>
2019-11-23 00:19:36 -0500

    vec! macro and Vec::with_capacity

--- src/learning_standard_lib/vectors.rs
+++ src/learning_standard_lib/vectors.rs
@@ -1,10 +1,17 @@
 /// Simple test with standard library vector, Vec
 pub fn test_vector() {
-    let mut vec = Vec::new();
+    let mut vec = Vec::with_capacity(2); // We know the size that we need.  Otherwise, we could have used Vec::new()
     vec.push("Hello");
     vec.push("World");
 
     for s in vec {
         println!("{}", s);
+    }
+
+    // Use vec! macro to initialize Vec with the array syntax
+    let v = vec![1, 2, 3];
+
+    for i in v {
+        println!("{}", i);
     }
 }
