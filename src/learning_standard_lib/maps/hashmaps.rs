6f328ebb25bda237e75ef04a8451553031391c82
Blankfrack <bryan.benoit@gmail.com>
2019-12-15 19:16:47 -0500

    Minor change

--- src/learning_standard_lib/maps/hashmaps.rs
+++ src/learning_standard_lib/maps/hashmaps.rs
@@ -9,7 +9,13 @@ pub fn test_hashmaps() {
     h.insert(String::from("Height"), String::from("5'8\""));
     h.insert(String::from("Weight"), String::from("185"));
 
+    for i in 1..100 {
+        h.insert(i.to_string(), i.to_string());
+    }
+
     for s in h.iter() {
         println!("{} - {}", s.0, s.1);
     }
+
+    println!("h.get(444) : {:#?}", h.get(&String::from("444")));
 }
